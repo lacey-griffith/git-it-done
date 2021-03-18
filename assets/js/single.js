3c46b74d957df7a44bd4821a5b137ddacba3a6e7
Lacey <lacey.griffith@g.austincc.edu>
2021-03-18 16:11:28 -0500

    display issues for indicated repo

---
+++ assets/js/single.js
@@ -0,0 +1,60 @@
+var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+function getRepoIssues(repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayIssues(data);
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            })
+        } else {
+            alert("There was a problem with your request!");
+        }
+    })
+};
+
+function displayIssues(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    for (var i = 0; i < issues.length; i++) {
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        issueEl.appendChild(titleEl);
+
+        var typeEl = document.createElement("span");
+
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull Request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+        issueEl.appendChild(typeEl);
+
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
+function displayWarning(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, please visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("lacey-griffith/git-it-done");
